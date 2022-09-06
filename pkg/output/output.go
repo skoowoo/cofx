819963acd2af9f7d258c24a83cc22ba2671c90f6
skoo <marckywu@gmail.com>
2022-09-06 14:34:20 +0800

    feature: added shell driver
    Signed-off-by: skoo <marckywu@gmail.com>

---
+++ pkg/output/output.go
@@ -0,0 +1,41 @@
+package output
+
+import (
+	"bytes"
+	"io"
+)
+
+type Output struct {
+	W          io.Writer
+	HandleFunc func(line []byte)
+	buffer     []byte
+}
+
+func (o *Output) Write(p []byte) (n int, err error) {
+	l := len(p)
+	for i := 0; i < l; {
+		end := bytes.IndexByte(p[i:], '\n')
+		if end != -1 {
+			line := p[i : i+end]
+			if len(o.buffer) > 0 {
+				line = append(o.buffer, line...)
+				o.buffer = nil
+			}
+			if o.HandleFunc != nil {
+				o.HandleFunc(line)
+			}
+			i = i + end + 1
+			continue
+		}
+		o.buffer = p[i:]
+		break
+	}
+	return o.W.Write(p)
+}
+
+func (o *Output) Close() {
+	if len(o.buffer) > 0 && o.HandleFunc != nil {
+		o.HandleFunc(o.buffer)
+	}
+	o.buffer = nil
+}
