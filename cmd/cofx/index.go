b750e676aa2beec03ede1d992d5aedf9b27e23a9
skoowu <skoowu.fancy@gmail.com>
2022-10-12 15:09:55 +0800

    refactor: use pretty framework for index of cli
    Signed-off-by: skoowu <skoowu.fancy@gmail.com>

--- cmd/cofx/index.go
+++ cmd/cofx/index.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/cofxlabs/cofx/config"
 	pretty "github.com/cofxlabs/cofx/pkg/pretty"
 	"github.com/cofxlabs/cofx/service"
@@ -55,61 +54,19 @@ func (m indexModel) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m indexModel) View() string {
-	// name
-	nameStyle := lipgloss.NewStyle().
-		Width(m.width).
-		Bold(true).
-		Italic(true).
-		Align(lipgloss.Center).
-		MarginTop(m.height / 10).
-		Foreground(lipgloss.Color("15"))
-	name := "cofx"
-	name = nameStyle.Render(name)
+	window := pretty.NewWindow(m.height, m.width, true)
+	window.SetTitle(pretty.NewTitleBlock("CoFx", pretty.ShadeText("Turn boring stuff into low code ...", 0), true))
 
-	// slogan
-	sloganStyle := lipgloss.NewStyle().
-		MarginTop(2).
-		Width(m.width).
-		Align(lipgloss.Center)
-	slogan := "Turn boring stuff into low code ..."
-	slogan = sloganStyle.Render(pretty.ShadeText(slogan, 0))
-
-	// metrics
-	functions := lipgloss.JoinVertical(lipgloss.Left, strconv.Itoa(m.functions), pretty.ColorGrey1.Render("Standard Functions"))
-	flows := lipgloss.JoinVertical(lipgloss.Left, strconv.Itoa(m.flows), pretty.ColorGrey1.Render("Available Flows"))
-	homeDir := lipgloss.JoinVertical(lipgloss.Left, m.homeDir, pretty.ColorGrey1.Render("CoFx Home Directory"))
-
-	maxWidth := max(lipgloss.Width(functions), lipgloss.Width(flows), lipgloss.Width(homeDir))
-	blockStyle := lipgloss.NewStyle().
-		BorderBottom(true).BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("99")).
-		Align(lipgloss.Left).
-		MarginLeft(0).MarginRight(1).
-		Width(maxWidth)
-	blockLayoutStyle := lipgloss.NewStyle().Margin(int(float64(m.height)*0.18), 1, 1, 1)
-	af := blockStyle.Copy().Render(flows)
-	af = blockLayoutStyle.Render(af)
-
-	sf := blockStyle.Copy().Render(functions)
-	sf = blockLayoutStyle.Render(sf)
-
-	hd := blockStyle.Copy().Render(homeDir)
-	hd = blockLayoutStyle.Render(hd)
-
-	metrics := lipgloss.JoinHorizontal(lipgloss.Bottom, af, sf, hd)
-	metrics = lipgloss.NewStyle().Width(m.width).Align(lipgloss.Center).Render(metrics)
-
-	// version
-	remainH := m.height - lipgloss.Height(name) - lipgloss.Height(slogan) - lipgloss.Height(metrics)
-
-	versionStyle := lipgloss.NewStyle().
-		MarginTop(remainH + 1).
-		Width(m.width).
-		Align(lipgloss.Center)
 	version := "v0.0.2"
-	from := "https://github.com/cofxlabs/cofx"
-	help := "Press any key to exit"
-	version = version + " " + from + " • " + help
-	version = versionStyle.Render(pretty.ColorGrey3.Render(version))
+	help := version + " https://github.com/cofxlabs/cofx • Press any key to exit"
+	window.SetFooter(pretty.NewFooterBlock(help))
 
-	return name + slogan + metrics + version
+	kvs := [][]string{
+		{"Standard Functions", strconv.Itoa(m.functions)},
+		{"Available Flows", strconv.Itoa(m.flows)},
+		{"Home Directory", m.homeDir},
+	}
+	window.AppendBlock(pretty.NewKvsBlock(kvs...))
+
+	return window.Render()
 }
